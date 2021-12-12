ac771e3d27db604a9e76a2028331ab4719483ffa
abdfnx <abdfnabd@gmail.com>
2021-12-12 08:17:47 +0000

    create `resto` extension builder

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+tag="${1}"
+versionDate="${2}"
+
+if [ "${tag}" == "" ]; then
+  echo "tag argument required"
+  exit 1
+fi
+
+if [ "${versionDate}" == "" ]; then
+  echo "versionDate argument required"
+  exit 1
+fi
+
+rm -rf resto
+rm -rf dist
+
+git clone https://github.com/abdfnx/resto
+cd resto
+go mod tidy
+
+GOOS=darwin  GOARCH=amd64 go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/darwin-x86_64-${tag}" 
+GOOS=linux   GOARCH=386   go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/linux-i386-${tag}"
+GOOS=linux   GOARCH=amd64 go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/linux-x86_64-${tag}"
+GOOS=windows GOARCH=386   go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/windows-i386-${tag}"
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/windows-x86_64-${tag}"
+
+mv dist ../

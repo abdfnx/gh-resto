78d9354274edd52ce8f40c0b490d4d731a7106fe
Abdfn <64256993+abdfnx@users.noreply.github.com>
2021-12-17 21:38:47 +0300

    remove tag from file names

--- build.sh
+++ build.sh
@@ -23,11 +23,11 @@ git clone https://github.com/abdfnx/resto rs
 cd rs
 go mod tidy
 
-GOOS=darwin  GOARCH=amd64 go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/darwin-x86_64-${tag}" 
-GOOS=linux   GOARCH=386   go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/linux-i386-${tag}"
-GOOS=linux   GOARCH=amd64 go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/linux-x86_64-${tag}"
-GOOS=windows GOARCH=386   go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/windows-i386-${tag}"
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/windows-x86_64-${tag}"
+GOOS=darwin  GOARCH=amd64 go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/darwin-x86_64" 
+GOOS=linux   GOARCH=386   go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/linux-i386"
+GOOS=linux   GOARCH=amd64 go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/linux-x86_64"
+GOOS=windows GOARCH=386   go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/windows-i386"
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=${tag} -X main.versionDate=${versionDate}" -o "dist/windows-x86_64"
 
 mv dist ../
 
